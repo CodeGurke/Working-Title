cb1c42385b119469db2d0a4917f63c306beaf6ca
SmileGuy <codegurke@gmail.com>
2024-10-29 12:59:32 +0100

    Simple Characterbewegung implementiert

--- working-title/src/player/player.gd
+++ working-title/src/player/player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 var speed : int = 500
+var jump_force : int = -2000
+var gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 
 func _physics_process(delta):
-	var movement : Vector2 = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
-	print(movement)
+	var movement : int = Input.get_axis('move_left', 'move_right')
+	var jump : bool = Input.is_action_pressed("move_up")
 	
-	velocity = movement * speed
+	velocity.x = movement * speed
+	if jump == true and self.is_on_floor(): velocity.y = jump_force
+	velocity.y += gravity
+	
 	move_and_slide()
