23bf5d3b477e16c23b348d40d17a164a409c83b5
SmileGuy <codegurke@gmail.com>
2024-11-05 14:51:44 +0100

    Frühes Character-Bau System, Bewegung Kaputt

---
+++ working-title/src/player/character.gd
@@ -0,0 +1,22 @@
+class_name Character
+extends Node2D
+
+
+# gets access to the part that is being used
+# TODO make the parts selectable and change here automatically
+const SELECTED_LEGS : = preload("res://src/player/parts/legs/legs_standart.tscn")
+const SELECTED_BODY : = preload("res://src/player/parts/body/body.tscn")
+ 
+# saves the instant of the part for later use
+var legs : CharacterBody2D = SELECTED_LEGS.instantiate()
+var body : CharacterBody2D = SELECTED_BODY.instantiate()
+
+
+# call at start to build the players character
+func build_body() -> void:
+	# build the character body from the legs up
+	%character.add_child(legs)
+	%character.add_child(body)
+	
+	# sets the pieces position to the connector of the last part
+	body.global_position = legs.get_connector_position()
