5b25cc599822df87899f5e128cb1f1d871551e3b
SmileGuy <codegurke@gmail.com>
2024-11-06 09:45:13 +0100

    Movement Bug gefixts

--- working-title/src/player/player.gd
+++ working-title/src/player/player.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
+
+# holds the player characters stats
+# TODO should probably be moved to the character Node
 var speed : int = 500
 var jump_force : int = -2000
 var gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+# when everything is loaded builds the players character
+func _ready() -> void:
+	%character.build_body()
 
+
+# movement code
 func _physics_process(delta):
 	var movement : int = Input.get_axis('move_left', 'move_right')
 	var jump : bool = Input.is_action_pressed("move_up")
--- working-title/src/state-machine/state-machine.gd
+++ working-title/src/state-machine/state-machine.gd
@@ -1,37 +1,37 @@
 extends Node
-
-
-@export var default_state : Node
-
-var states : Array[Node] = [$idle, $move, $jump, $fall]
-var active_states : Array[Node]
-
-
-
-func _init():
-	active_states.append(default_state)
-
-func process_frame() -> void:
-	pass
-
-func process_physics() -> void:
-	active_states.clear()
-	
-	var movement : float = get_movement()
-	var jump : bool = get_jump()
-	
-	if movement == 0 and jump == false:
-		active_states.append($idle)
-		return
-	
-	if movement != 0:
-		active_states.append($move)
-	
-	if jump == true:
-		active_states.append($jump)
-
-func get_movement() -> float:
-	return Input.get_axis('move_left', 'move_right')
-
-func get_jump() -> bool:
-	return Input.is_action_pressed("move_up")
+# disabled because it doesnt yet have an use
+#
+#@export var default_state : Node
+#
+#var states : Array[Node] = [$idle, $move, $jump, $fall]
+#var active_states : Array[Node]
+#
+#
+#
+#func _init():
+	#active_states.append(default_state)
+#
+#func process_frame() -> void:
+	#pass
+#
+#func process_physics() -> void:
+	#active_states.clear()
+	#
+	#var movement : float = get_movement()
+	#var jump : bool = get_jump()
+	#
+	#if movement == 0 and jump == false:
+		#active_states.append($idle)
+		#return
+	#
+	#if movement != 0:
+		#active_states.append($move)
+	#
+	#if jump == true:
+		#active_states.append($jump)
+#
+#func get_movement() -> float:
+	#return Input.get_axis('move_left', 'move_right')
+#
+#func get_jump() -> bool:
+	#return Input.is_action_pressed("move_up")
