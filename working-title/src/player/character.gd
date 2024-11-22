348d2079928eecf29e8d58d2e4acd66fe6597e81
SmileGuy <codegurke@gmail.com>
2024-11-22 10:56:11 +0100

    Hände hinzugefügt

--- working-title/src/player/character.gd
+++ working-title/src/player/character.gd
@@ -7,11 +7,15 @@ extends Node2D
 const SELECTED_LEGS : = preload("res://src/player/parts/legs/legs_standart.tscn")
 const SELECTED_BODY : = preload("res://src/player/parts/body/body.tscn")
 const SELECTED_HEAD : = preload("res://src/player/parts/head/head.tscn")
+const SELECTED_HAND_L : = preload("res://src/player/parts/hands/hand.tscn")
+const SELECTED_HAND_R : = preload("res://src/player/parts/hands/hand.tscn")
  
 # saves the instant of the part for later use
 var legs : CharacterBody2D = SELECTED_LEGS.instantiate()
 var body : CharacterBody2D = SELECTED_BODY.instantiate()
 var head : CharacterBody2D = SELECTED_HEAD.instantiate()
+var hand_l : CharacterBody2D = SELECTED_HAND_L.instantiate()
+var hand_r : CharacterBody2D = SELECTED_HAND_R.instantiate()
 
 
 # call at start to build the players character
@@ -20,7 +24,13 @@ func build_body() -> void:
 	%character.add_child(legs)
 	%character.add_child(body)
 	%character.add_child(head)
+	%character.add_child(hand_l)
+	%character.add_child(hand_r)
 	
 	# sets the pieces position to the connector of the last part
 	body.global_position = legs.get_connector_position()
 	head.global_position = body.get_connector_position()
+	hand_l.global_position = legs.get_connector_position()
+	hand_l.global_position.x += 30
+	hand_r.global_position = legs.get_connector_position()
+	hand_r.global_position.x -= 50
