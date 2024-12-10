496d436a94b7b5ffc74144abd6c9e7aa3d525d2f
SmileGuy <codegurke@gmail.com>
2024-12-10 14:09:28 +0100

    An Modularität gearbeitet & arbeit an hurtboxen

--- working-title/src/player/character.gd
+++ working-title/src/player/character.gd
@@ -1,25 +1,32 @@
 class_name Character
 extends Node2D
 
-
-# gets access to the part that is being used
-# TODO make the parts selectable and change here automatically
-const SELECTED_LEGS : = preload("res://src/player/parts/legs/legs_standart.tscn")
-const SELECTED_BODY : = preload("res://src/player/parts/body/body.tscn")
-const SELECTED_HEAD : = preload("res://src/player/parts/head/head.tscn")
-const SELECTED_HAND_L : = preload("res://src/player/parts/hands/hand.tscn")
-const SELECTED_HAND_R : = preload("res://src/player/parts/hands/hand.tscn")
  
-# saves the instant of the part for later use like calling functions
-var legs : CharacterBody2D = SELECTED_LEGS.instantiate()
-var body : CharacterBody2D = SELECTED_BODY.instantiate()
-var head : CharacterBody2D = SELECTED_HEAD.instantiate()
-var hand_l : CharacterBody2D = SELECTED_HAND_L.instantiate()
-var hand_r : CharacterBody2D = SELECTED_HAND_R.instantiate()
+# holds the instances of the individual body parts to access later using code
+var legs : CharacterBody2D
+var body : CharacterBody2D
+var head : CharacterBody2D
+var hand_l : CharacterBody2D
+var hand_r : CharacterBody2D
 
 
-# call at start to build the players character
+# is processed every frame
+func _process(delta):
+	update_position()
+
+
+# this function is called by the player script to build the players character
 func build_body() -> void:
+	# selected parts are stored in player and accessed here
+	var parts : Array[PackedScene] = get_parent().get_selected_parts()
+	
+	# instantiates every part of the players body
+	legs = parts[0].instantiate()
+	body = parts[1].instantiate()
+	head = parts[2].instantiate()
+	hand_l = parts[3].instantiate()
+	hand_r = parts[4].instantiate()
+	
 	# build the character body from the legs up
 	%character.add_child(legs)
 	%character.add_child(body)
@@ -27,9 +34,15 @@ func build_body() -> void:
 	%character.add_child(hand_l)
 	%character.add_child(hand_r)
 	
+	# connects the hurtboxes of the individual body parts to a function
+	$legs/Area2D.area_entered.connect(hitbox_entered_hurtbox.bind($legs/Area2D))
+	$body/Area2D.area_entered.connect(hitbox_entered_hurtbox.bind($body/Area2D))
+	$head/Area2D.area_entered.connect(hitbox_entered_hurtbox.bind($head/Area2D))
+	
 	update_position()
 
 
+# this is used to change the rest of the body to movement of the piece the layer down
 func update_position() -> void:
 	body.global_position = legs.get_connector_position()
 	head.global_position = body.get_connector_position()
@@ -39,5 +52,6 @@ func update_position() -> void:
 	hand_r.global_position.x -= 50
 
 
-func _process(delta):
-	update_position()
+# function to check the hitbox that entered a hurtbox / ! unclear if hurtbox is assigned
+func hitbox_entered_hurtbox(hitbox : Area2D, hurtbox : Area2D) -> void:
+	pass
--- working-title/src/player/player.gd
+++ working-title/src/player/player.gd
@@ -2,14 +2,19 @@ extends CharacterBody2D
 
 
 # holds the player characters stats
-# TODO should probably be moved to the character Node
+# TODO the next 3 lines should probably be moved to the character Node
 var speed : int = 500
 var jump_force : int = -2000
 var gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+# gets access to the part that is being used
+# TODO make the parts selectable and change here automatically
+var selected_parts : Array[PackedScene]
+
 # when everything is loaded builds the players character
 func _ready() -> void:
+	test_auto()
 	%character.build_body()
 
 
@@ -23,3 +28,16 @@ func _physics_process(delta):
 	velocity.y += gravity
 	
 	move_and_slide()
+
+
+func select_parts(legs : PackedScene, body : PackedScene, head : PackedScene, hand_l : PackedScene, hand_r : PackedScene) -> void:
+	selected_parts = [legs, body, head, hand_l, hand_r]
+
+
+func get_selected_parts() -> Array[PackedScene]:
+	return selected_parts
+
+
+# this function is for testing purposeses because a character builder is not built yet
+func test_auto() -> void:
+	select_parts(preload("res://src/player/parts/legs/legs_standart.tscn"), preload("res://src/player/parts/body/body.tscn"), preload("res://src/player/parts/head/head.tscn"), preload("res://src/player/parts/hands/hand.tscn"), preload("res://src/player/parts/hands/hand.tscn"))
