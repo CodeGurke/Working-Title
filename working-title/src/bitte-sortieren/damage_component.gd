a46558f9d24dba9e5eea775288a804d317b3510b
SmileGuy <codegurke@gmail.com>
2025-04-01 14:02:13 +0200

    Revert "Merge pull request #9 from CodeGurke/cr"
    This reverts commit 814acfb9e4dfcec7c9508e4258704e7044fa3d96, reversing
    changes made to d0bdca90372cad1257a6520dd756348ddc417b27.

--- working-title/src/bitte-sortieren/damage_component.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-var damagetaken
-var knockback
-func _ready():
-	connect("hit",self.take_damage)
-
-func take_damage(damage:int) -> void:
-	print("ouch, " + str(damage) + " damage!")
-	damagetaken += damage
--- working-title/src/player/hitboxes/hitbox.gd
+++
@@ -1,9 +0,0 @@
-class_name HitBox
-extends Area2D
-
-@export var damage := 10
-@export var team := 0
-
-func _init() -> void:
-	collision_layer = 1
-	collision_mask = 1
