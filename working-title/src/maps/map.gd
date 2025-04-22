192a18142cee02f339666ad6e5c37111d9260f82
SmileGuy <codegurke@gmail.com>
2025-04-22 14:14:57 +0200

    Working on detecting player leaving area

--- working-title/src/maps/map.gd
+++ working-title/src/maps/map.gd
@@ -2,6 +2,7 @@ class_name Map
 extends Node2D
 # This is the Map class used for all maps
 
+signal player_left_map(player : CharacterBody2D)
 
 # this function dynamically gets the name and position of player spawn points that are children of the node spawn_locations
 func get_spawn_locations() -> Dictionary:
@@ -11,3 +12,7 @@ func get_spawn_locations() -> Dictionary:
 	for entry in location_markers:
 		spawn_locations.get_or_add(entry.name, entry.get_global_position())
 	return spawn_locations
+
+
+func _on_player_zone_body_exited(body : CharacterBody2D):
+	player_left_map.emit(body)
